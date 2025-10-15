65aab8e83be35091b1c3ea1ef4b52d55ed43f6c6
Jyothish T M <jothishtm2002@gmail.com>
2025-10-15 16:23:48 +0530

    error fixed

--- src/api/ocrService.js
+++ src/api/ocrService.js
@@ -1,19 +1,7 @@
 import axios from 'axios'
 
-// export const runOcr = async ({ front, back, signal }) => {
-//   const formData = new FormData()
-//   formData.append('front', front)
-//   formData.append('back', back)
 
-//   const { data } = await axios.post('/api/ocr', formData, {
-//     signal,
-//     headers: { 'Content-Type': 'multipart/form-data' },
-//   })
-
-//   return data
-// }
-
-const API_BASE = 'http://localhost:5000'
+const API_BASE = import.meta.env.VITE_API_BASE_URL 
 
 export const runOcr = async ({ front, back, signal }) => {
   const formData = new FormData()
